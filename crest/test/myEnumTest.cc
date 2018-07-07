fc356a5d089cf2cd2c22b2512409f7adc890429d
reza ahmadi <re.ahmdi@gmail.com>
2018-07-06 21:18:55 -0400

    fixed an issue in loop bound

--- crest/test/myEnumTest.cc
+++ crest/test/myEnumTest.cc
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -35,6 +36,14 @@ int main(){
 	 for (int i=0;i<10;i++){
                 printf ("from vector: %d", ids.at(i));   
         }
+	
+	 printf ("\ncompute start");   
+	double x=0;
+	for (double i=0;i<100000000;i++){
+		x += sqrt(sqrt(sqrt(i)));
+	}
+printf ("\ncompute end");   
+
 
 //      getEnum(END);
         return 0;
